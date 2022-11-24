bfa0a57c5b9acf5ca3716ae0915f57b7f23db02f
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-24 06:05:07 -0600

    asgsag

--- variables.tf
+++ variables.tf
@@ -3,3 +3,23 @@ variable "static_site_name" {
   default     = "Test PR"
   description = "Static Site Name for Resume Client"
 }
+
+variable "client_secret" {
+  type        = string
+  default     = ""
+}
+
+variable "client_id" {
+  type        = string
+  default     = ""
+}
+
+variable "tenant_id" {
+  type        = string
+  default     = ""
+}
+
+variable "subscription_id" {
+  type        = string
+  default     = ""
+}
