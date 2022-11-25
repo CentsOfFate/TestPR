b7ccb262c674414f30f8c4c3f67a37eda14c50f2
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-25 08:10:00 -0600

    sfa

--- azure_static_site.tf
+++ azure_static_site.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_site" "site" {
-  name                = "test-pr"
+  name                = "test-pr2"
   resource_group_name = "crc-dev"
   location            = "Central US"
   sku_tier            = "Free"
